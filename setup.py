7f16a11ea5220703fd6865ab6929417bfa362cbb
Tobias Gleißner <tobias.gleissner@fu-berlin.de>
2019-09-24 10:38:44 +0200

    removed invalid import

--- setup.py
+++ setup.py
@@ -1,16 +1,36 @@
 import pathlib
-from setuptools import setup
-from setuptools import find_packages
+from setuptools import setup, find_packages
 
 HERE = pathlib.Path(__file__).parent
-README = (HERE / "README.md").read_text()
+README_CONTENT = (HERE / "README.md").read_text(encoding='utf-8')
+
 
 setup(name='tptp',
-    version='0.0.1',
+    version='0.0.3-dev2',
     description='A library for handling TPTP related input and systems',
+    description_content_type='text/plain',
+    long_description=README_CONTENT,
+    long_description_content_type='text/markdown',
+    author='Tobias Gleißner',
+    author_email='tobias.gleissner@fu-berlin.de',
+    url='https://github.com/leoprover/tptp',
     classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: Legal Industry',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
+    python_requires='>=3.5',
     #keywords='TPTP',
     license='BSD3',
     packages=find_packages(),
--- tptp/reasoning/__init__.py
+++ tptp/reasoning/__init__.py
@@ -2,5 +2,4 @@ from .core.solver import Solver
 from .core.solverCall import SolverCall
 from .core.solverResult import SolverResult
 
-from .scheduler.reasoningScheduler import ReasoningScheduler
 from .loader import loadSolvers, getLocalSolvers, getSystemOnTptpSolvers, getLocalSolver, getSystemOnTptpSolver
